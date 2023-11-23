80952613d26613ad1b8c7982af436bf0a650dbd1
Tejodbhav Koduru <tejkodur@gmail.com>
2023-11-23 14:05:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    document.getElementById('content').innerText = data.articles[1].tags.concern;
-    
+    const contentDiv = document.getElementById('content');
+    data.articles.forEach(article => {
+      const articleDiv = document.createElement('div');
+      articleDiv.className = 'article';
+      articleDiv.innerHTML = `
+        <h2>${article.title}</h2>
+        <p>${article.summary}</p>
+        <div class="tags">
+          Length: ${article.tags.length}, Concern: ${article.tags.concern}
+        </div>
+      `;
+      contentDiv.appendChild(articleDiv);
+    });
   });
+
