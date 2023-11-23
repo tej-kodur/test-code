4b9959360b8722baccb21463c92a74807109109d
Tejodbhav Koduru <tejkodur@gmail.com>
2023-11-23 05:03:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    document.getElementById('content').innerText = data.articles[0].magic;
+    document.getElementById('content').innerText = data.articles[0].title;
   });
